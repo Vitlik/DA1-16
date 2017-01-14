b692c6061c8af96bc049c4de1c53d0eb9dc65b49
Vit <vit@Lap.v>
2017-01-14 22:56:32 +0100

    Example Documentation

--- R/util.R
+++ R/util.R
@@ -3,6 +3,21 @@
 # output folder for all the created information (text, tables & plots)
 output_folder = "../out"
 
-test.func <- function(x){
-  return(x*x)
+#' @title Example Function
+#'
+#' @description
+#' Example function for understanding documentation
+#' Calculates product of \code{x^2} multiplied by \code{y}
+#'
+#' @param x A number which will be squared
+#' @param y A number which get multiplied to \code{x^2}
+#' @return the caculated result of \code{x * x * y}
+#' @examples
+#' test.func(3,2)
+#' # 18
+#'
+#' test.func(2,3)
+#' # 12
+test.func <- function(x, y){
+  return(x*x*y)
 }
