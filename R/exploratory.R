f26e9ea533d099c5a8b404ee20e82ce4c3f4214b
Vit <vit@Lap.v>
2017-01-20 15:48:35 +0100

    environment variable adjusted & out folder created if not existing

--- R/exploratory.R
+++ R/exploratory.R
@@ -12,41 +12,48 @@ exploratory.data_analysis <- function(){
 #' @title Structure the spambase data
 #' @description Create an environment variable \code{stuff} which
 exploratory.structure_data <- function(){
+  e <<- baseenv()
+
+  # denotes whether the e-mail was considered spam (1) or not (0)
+  e$classes <- spambase[, 58, drop = F]
+  e$noclasses <- spambase[, -58, drop = F]
+  e$allspam <- spambase[spambase[,58]==1, -58]
+  e$allnospam <- spambase[spambase[,58]==0, -58]
 
   # continuous numeric values which describe the occurence of a word in \code{%}
-  stuff.word_freq <<- spambase[, 1:48]
-  stuff.word_freq.spam <<- spambase[spambase[,58]==1, 1:48]
-  stuff.word_freq.nospam <<- spambase[spambase[,58]==0, 1:48]
+  e$word_freq <- spambase[, 1:48]
+  e$word_freq.spam <- spambase[spambase[,58]==1, 1:48]
+  e$word_freq.nospam <- spambase[spambase[,58]==0, 1:48]
 
   # continuous numeric values which describe the occurence of a special character in \code{%}
-  stuff.char_freq <<- spambase[, 49:54]
-  stuff.char_freq.spam <<- spambase[spambase[,58]==1, 49:54]
-  stuff.char_freq.nospam <<- spambase[spambase[,58]==0, 49:54]
+  e$char_freq <- spambase[, 49:54]
+  e$char_freq.spam <- spambase[spambase[,58]==1, 49:54]
+  e$char_freq.nospam <- spambase[spambase[,58]==0, 49:54]
 
   # continuous numeric value which describe the average length of uninterrupted sequences of
   #   capital letters in \code{%}
-  stuff.CAP.length_avg <<- spambase[, 55, drop = F]
-  stuff.CAP.length_avg.spam <<- spambase[spambase[,58]==1, 55, drop = F]
-  stuff.CAP.length_avg.nospam <<- spambase[spambase[,58]==0, 55, drop = F]
+  e$CAP.length_avg <- spambase[, 55, drop = F]
+  e$CAP.length_avg.spam <- spambase[spambase[,58]==1, 55, drop = F]
+  e$CAP.length_avg.nospam <- spambase[spambase[,58]==0, 55, drop = F]
 
   # discret numeric value which describe the length of longest uninterrupted sequence of
   #   capital letters
-  stuff.CAP.length_longest <<- spambase[, 56, drop = F]
-  stuff.CAP.length_longest.spam <<- spambase[spambase[,58]==1, 56, drop = F]
-  stuff.CAP.length_longest.nospam <<- spambase[spambase[,58]==0, 56, drop = F]
+  e$CAP.length_longest <- spambase[, 56, drop = F]
+  e$CAP.length_longest.spam <- spambase[spambase[,58]==1, 56, drop = F]
+  e$CAP.length_longest.nospam <- spambase[spambase[,58]==0, 56, drop = F]
 
   # discret numeric value which describe the total number of capital letters in the e-mail
-  stuff.CAP.length_total <<- spambase[, 57, drop = F]
-  stuff.CAP.length_total.spam <<- spambase[spambase[,58]==1, 57, drop = F]
-  stuff.CAP.length_total.nospam <<- spambase[spambase[,58]==0, 57, drop = F]
-
-  # denotes whether the e-mail was considered spam (1) or not (0)
-  stuff.classification <<- spambase[, 58, drop = F]
+  e$CAP.length_total <- spambase[, 57, drop = F]
+  e$CAP.length_total.spam <- spambase[spambase[,58]==1, 57, drop = F]
+  e$CAP.length_total.nospam <- spambase[spambase[,58]==0, 57, drop = F]
 }
 
 #' @title Create exploratory plots
 #' @description
 exploratory.create_plots <- function(){
+  # create outputfolder if not existing yet
+  if(!file.exists("out")) dir.create("out")
+
   # Write information about the structure of the data into a file
   target <- file.path("out/1. Exploratory - str.txt")
   file.create(target)
@@ -61,11 +68,11 @@ exploratory.create_plots <- function(){
   #
   target <- file.path("out/1. Exploratory - summary - spam.csv")
   file.create(target)
-  write.csv2(summary(spambase[(spambase[,58] == 1), -58]), target)
-
+  write.csv2(summary(allspam), target)
+  #
   target <- file.path("out/1. Exploratory - summary - nospam.csv")
   file.create(target)
-  write.csv2(summary(spambase[(spambase[,58] == 0), -58]), target)
+  write.csv2(summary(allnospam), target)
 
   #
   pdf(file.path("out/1. Exploratory - Histograms.pdf"))
--- R/util.R
+++ R/util.R
@@ -1,4 +1,27 @@
 
+#'
+#'
+util.gen.summary <- function(frame1, frame2, file){
+  stopifnot()
+  if(missing(frame2)){
+
+  } else {
+
+  }
+}
+
+# set some colors
+ercis.black    = rgb(  0/255,   0/255,   0/255, 1)
+ercis.grey     = rgb( 94/255,  94/255,  93/255, 1)
+ercis.red      = rgb(133/255,  35/255,  57/255, 1)
+ercis.lightred = rgb(200/255, 156/255, 166/255, 1)
+ercis.blue     = rgb(135/255, 151/255, 163/255, 1)
+ercis.darkblue = rgb( 67/255,  92/255, 139/255, 1)
+ercis.cyan     = rgb(  0/255, 156/255, 179/255, 1)
+ercis.orange   = rgb(231/255, 124/255,  18/255, 1)
+ercis.green    = rgb(135/255, 191/255,  42/255, 1)
+
+
 #' @title Example Function for docu creation
 #'
 #' @description
@@ -17,14 +40,3 @@
 util.test.func <- function(x, y){
   return(x*x*y)
 }
-
-# set some colors
-ercis.black    = rgb(  0/255,   0/255,   0/255, 1)
-ercis.grey     = rgb( 94/255,  94/255,  93/255, 1)
-ercis.red      = rgb(133/255,  35/255,  57/255, 1)
-ercis.lightred = rgb(200/255, 156/255, 166/255, 1)
-ercis.blue     = rgb(135/255, 151/255, 163/255, 1)
-ercis.darkblue = rgb( 67/255,  92/255, 139/255, 1)
-ercis.cyan     = rgb(  0/255, 156/255, 179/255, 1)
-ercis.orange   = rgb(231/255, 124/255,  18/255, 1)
-ercis.green    = rgb(135/255, 191/255,  42/255, 1)
